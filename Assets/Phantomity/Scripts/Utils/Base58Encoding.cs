f27483c5a4eb766f2a19a97f05604625c2b84e7a
sidorovk <kiryha_rigel@mail.ru>
2023-11-19 18:48:12 +0800

    Apply code style to Base58Encoding

--- Assets/Phantomity/Scripts/Utils/Base58Encoding.cs
+++ Assets/Phantomity/Scripts/Utils/Base58Encoding.cs
@@ -5,74 +5,65 @@ using System.Security.Cryptography;
 
 namespace Phantomity.Utils
 {
-    // Implements https://en.bitcoin.it/wiki/Base58Check_encoding
     public static class Base58Encoding
     {
-        public const int CheckSumSizeInBytes = 4;
-        
+        private const int CheckSumSizeInBytes = 4;
         private const string Digits = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
         public static string Encode(byte[] data)
         {
-            // Decode byte[] to BigInteger
-            BigInteger intData = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-                intData = intData * 256 + data[i];
-            }
+            var intData = GetDataInt(data);
 
-            // Encode BigInteger to Base58 string
-            string result = "";
+            var result = "";
             while (intData > 0)
             {
-                int remainder = (int)(intData % 58);
+                var remainder = (int)(intData % 58);
                 intData /= 58;
                 result = Digits[remainder] + result;
             }
 
-            // Append `1` for each leading 0 byte
-            for (int i = 0; i < data.Length && data[i] == 0; i++)
+            for (var i = 0; i < data.Length && data[i] == 0; i++)
             {
                 result = '1' + result;
             }
             return result;
         }
 
-    
         public static byte[] Decode(string s)
         {
-            // Decode Base58 string to BigInteger 
             BigInteger intData = 0;
-            for (int i = 0; i < s.Length; i++)
+            for (var i = 0; i < s.Length; i++)
             {
-                int digit = Digits.IndexOf(s[i]); //Slow
+                var digit = Digits.IndexOf(s[i]);
                 if (digit < 0)
-                    throw new FormatException(string.Format("Invalid Base58 character `{0}` at position {1}", s[i], i));
+                    throw new FormatException($"Invalid Base58 character {s[i]} at position {i}");
                 intData = intData * 58 + digit;
             }
-
-            // Encode BigInteger to byte[]
-            // Leading zero bytes get encoded as leading `1` characters
-            int leadingZeroCount = s.TakeWhile(c => c == '1').Count();
+            
+            var leadingZeroCount = s.TakeWhile(c => c == '1').Count();
             var leadingZeros = Enumerable.Repeat((byte)0, leadingZeroCount);
-            var bytesWithoutLeadingZeros =
-                intData.ToByteArray()
-                .Reverse()// to big endian
-                .SkipWhile(b => b == 0);//strip sign byte
+            var bytesWithoutLeadingZeros = intData.ToByteArray()
+                .Reverse() // to big endian
+                .SkipWhile(b => b == 0); //strip sign byte
             var result = leadingZeros.Concat(bytesWithoutLeadingZeros).ToArray();
             return result;
         }
 
-        // Throws `FormatException` if s is not a valid Base58 string, or the checksum is invalid
-        
+        private static BigInteger GetDataInt(byte[] data)
+        {
+            BigInteger intData = 0;
+            foreach (var digit in data)
+            {
+                intData = intData * 256 + digit;
+            }
+            return intData;
+        }
 
         private static byte[] GetCheckSum(byte[] data)
         {
-
-
-            SHA256 sha256 = new SHA256Managed();
-            byte[] hash1 = sha256.ComputeHash(data);
-            byte[] hash2 = sha256.ComputeHash(hash1);
+            var sha256 = new SHA256Managed();
+            var hash1 = sha256.ComputeHash(data);
+            var hash2 = sha256.ComputeHash(hash1);
 
             var result = new byte[CheckSumSizeInBytes];
             Buffer.BlockCopy(hash2, 0, result, 0, result.Length);
